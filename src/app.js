122f590e92d95d9e5c6458a779fcb7bd535195e0
GiselePorto <gisele.porto18@gmail.com>
2023-06-21 17:15:52 -0300

    atualizar

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import  express  from 'express'; // Importamos biblioteca
-import index from './routes/index.js';
+const  express = require ('express'); // Importamos biblioteca
+const index = require ('./routes/index.js');
 
 const app = express(); // esta variável recebe o express e será usada em outros arquivos.
 
@@ -9,4 +9,4 @@ const app = express(); // esta variável recebe o express e será usada em outro
 app.use(express.json()); //Começo da conexão para as rotas.
 app.use('/', index); //Primeira rota que teremos acesso.
 
-export default app //Agora estamos exportando arquivos após importá-los
+module.exports = app; //Agora estamos exportando arquivos após importá-los
